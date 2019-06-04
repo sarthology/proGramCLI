6bf0d8a8c61704b76273a39830bc346bc70d11d9
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2019-06-04 18:25:42 +0530

    [add] eslint and prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+	endOfLine: 'lf'
+};
