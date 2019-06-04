2ba2b747c058c41c4baaa726456e8feb82146023
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2019-06-04 19:06:33 +0530

    [add] cli launcher

---
+++ bin/cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+const electron = require('electron');
+const path = require('path');
+
+const serverPath = path.join(__dirname, '../main/main.js');
+
+const args = [serverPath]
+	.concat([].concat(process.argv).splice(2))
+	.concat('--not-packaged=true');
+
+const proc = spawn(electron, args, { stdio: 'inherit' });
+proc.on('close', code => {
+	process.exit(code);
+});
