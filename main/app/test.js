f2577255a4b90477a5d1ce9424420465c069955a
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2019-06-30 18:00:50 +0530

    remove accidently committed test.js

--- main/app/test.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const fs = require('fs-extra');
-const os = require('os');
-
-const dir = path.resolve(__dirname, '..', '..', 'program');
-const homedir = os.homedir();
-const outputDir = path.resolve(homedir, 'program');
-
-fs.copySync(dir, outputDir);
